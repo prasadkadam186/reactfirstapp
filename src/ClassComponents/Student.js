a3869fa3947f0d44c7b416a3d6287bc11e1d2bbd
Prasad Kadam <118296796+prasadkadam186@users.noreply.github.com>
2025-08-20 08:26:15 +0530

    props with class component

--- src/ClassComponents/Student.js
+++ src/ClassComponents/Student.js
@@ -1,6 +1,7 @@
 
 // Class based components Via seperate file
 import { Component } from "react";
+import {Subject} from "./Subject"
 
 export default class Student extends Component{
     // Purpose : Show the student data on click of show data button
@@ -13,7 +14,8 @@ export default class Student extends Component{
         this.Student={
             name : 'Samarth',
             age : '25',
-            address : 'Pune'
+            address : 'Pune',
+            subjectName : 'Marathi'
         }
     }
     render()
@@ -22,6 +24,7 @@ export default class Student extends Component{
             <div>
                  <h1>Hey! Its Student Class based components</h1>
                  <button onClick={()=>{this.showData()}}>Show Student Data</button>
+                 <Subject subjectName={this.Student.subjectName}/>
             </div>
         );
     }
---
+++ src/ClassComponents/Subject.js
@@ -0,0 +1,11 @@
+import React from "react";
+export class Subject extends React.Component{
+    render()
+    {
+        return(
+            <div>
+                <h1 style={{backgroundColor:'red', margin:'1%'}}>Sub : {this.props.subjectName}</h1>
+            </div>
+        )
+    }
+}

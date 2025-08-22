7d916510854874dcfb777ad6d7e1b1ecbe426947
Prasad Kadam <118296796+prasadkadam186@users.noreply.github.com>
2025-08-22 08:20:16 +0530

    Show, Hide and toggle button functionality completed for Subject component

--- src/ClassComponents/Subject.js
+++ src/ClassComponents/Subject.js
@@ -1,11 +1,29 @@
 // Prpos with class based components
 import React from "react";
 export class Subject extends React.Component{
+    constructor()
+    {
+        super();
+       this.state={
+        status:false
+       };
+    }
     render()
     {
         return(
             <div>
-                <h1 style={{backgroundColor:'red', margin:'1%'}}>Sub : {this.props.subjectName}</h1>
+                {/* Show and hide the data by state on click of below buttons */}
+                <button onClick={()=>this.setState({status: !this.state.status})}>Toggle</button>
+                <button onClick={()=>this.setState({status:true})} style={{backgroundColor:'green', margin:'1%'}}>Show</button>
+                <button onClick={()=>this.setState({status:false})} style={{backgroundColor:'Blue', margin:'1%'}}>Hide</button>
+                {
+                    this.state.status ? 
+                    (<div>
+                        <h4>This is Subject class based component called </h4>
+                        <h1 style={{backgroundColor:'red', margin:'1%'}}>Sub : {this.props.subjectName}</h1>
+                    </div>) 
+                    : null
+                }
             </div>
         )
     }
